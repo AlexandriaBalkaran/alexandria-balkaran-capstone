28e49d79b1fa7851b6040995fd009b04a6c0154f
Alexandria Balkaran <alexandriabalkaran@gmail.com>
2025-03-18 19:53:53 -0400

    add font mixins

--- pages/Homepage/Homepage.jsx
+++ pages/Homepage/Homepage.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import axios from "axios";
 import NeighbourhoodFilter from "../../components/NeighbourhoodFilter/NeighbourhoodFilter";
 import VenueCard from "../../components/VenueCard/VenueCard";
-
+import "./Homepage.scss";
 
 function HomePage() {
   return (
