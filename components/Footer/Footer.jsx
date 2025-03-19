18a19d6a085f3679bc6fe4a46fd344dfc6bb2b42
Alexandria Balkaran <alexandriabalkaran@gmail.com>
2025-03-18 23:58:00 -0400

    basic footer complete with social icons

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -11,25 +11,25 @@ const Footer = () => {
         </div>
         <div className="social__icons">
           <a href="https://www.facebook.com/">
-            <img
+            <img className="social__icons-img"
               src="../src/assets/images/Facebook.svg"
               alt="facebook icon"
             ></img>
           </a>
           <a href="https://x.com/?lang=en">
-            <img
+            <img className="social__icons-img"
               src="../src/assets/images/X_twitter.svg"
               alt="twitter icon"
             ></img>
           </a>
           <a href="https://www.instagram.com/">
-            <img
+            <img className="social__icons-img"
               src="../src/assets/images/Instagram.svg"
               alt="instagram icon"
             ></img>
           </a>
           <a href="https://www.pinterest.com/">
-            <img
+            <img className="social__icons-img"
               src="../src/assets/images/Pinterest.svg"
               alt="pinterest icon"
             ></img>
