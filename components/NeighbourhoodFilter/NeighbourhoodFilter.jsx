6e838f790ce2da19fcb79ce48eb43403947a2e09
Alexandria Balkaran <alexandriabalkaran@gmail.com>
2025-03-18 23:24:59 -0400

    day filter not working

--- components/NeighbourhoodFilter/NeighbourhoodFilter.jsx
+++ components/NeighbourhoodFilter/NeighbourhoodFilter.jsx
@@ -1,69 +1,16 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import "./NeighbourhoodFilter.scss";
+// import React, { useEffect, useState } from "react";
+// import { Link } from "react-router-dom";
+// import "./NeighbourhoodFilter.scss";
 
-// const NeighbourhoodFilter = () => {
-//   const [venues, setVenues] = useState([]);
-//   const [neighbourhoods, setNeighbourhoods] = useState([]);
-//   const [selectedNeighbourhood, setSelectedNeighbourhood] = useState("");
-
-//   useEffect(() => {
-//     fetchVenues();
-//   }, []);
-
-//   const fetchVenues = async () => {
-//     try {
-//       const response = await fetch("http://localhost:8080/venues");
-//       const data = await response.json();
-//       setVenues(data);
-//       setNeighbourhoods([...new Set(data.map(venue => venue.neighbourhood))]);
-//     } catch (e) {
-//       console.error("Error fetching venues:", e);
-//     }
-//   };
-
-//   const handleFilterChange = (event) => {
-//     setSelectedNeighbourhood(event.target.value);
-//   };
-
-//   const filteredVenues = selectedNeighbourhood
-//     ? venues.filter(venue => venue.neighbourhood === selectedNeighbourhood)
-//     : venues;
-
+// const NeighbourhoodFilter = ({ neighbourhoods, selectedNeighbourhood, onFilterChange }) => {
 //   return (
-//     <div>
-//       {/* <label htmlFor="neighbourhood">Select Neighbourhood:</label>
-//       <select id="neighbourhood" value={selectedNeighbourhood} onChange={handleFilterChange}>
-//         <option value="">All Neighbourhoods</option>
-//         {neighbourhoods.map((neighbourhood, index) => (
-//           <option key={index} value={neighbourhood}>{neighbourhood}</option>
-//         ))}
-//       </select> */}
-
-//       <ul>
-//         {filteredVenues.map((venue) => (
-//           <li key={venue.id}>
-//             <h3>{venue.name}</h3>
-//             <h4>{venue.neighbourhood}</h4>
-//             <Link to={`/venue/${venue.id}/deals`}>
-//               <img src={venue.photo} alt={venue.name} />
-//             </Link>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
+//     <select value={selectedNeighbourhood} onChange={onFilterChange}>
+//       <option value="">All Neighbourhoods</option>
+//       {neighbourhoods.map((neighbourhood, index) => (
+//         <option key={index} value={neighbourhood}>{neighbourhood}</option>
+//       ))}
+//     </select>
 //   );
 // };
 
-const NeighbourhoodFilter = ({ neighbourhoods, selectedNeighbourhood, onFilterChange }) => {
-  return (
-    <select value={selectedNeighbourhood} onChange={onFilterChange}>
-      <option value="">All Neighbourhoods</option>
-      {neighbourhoods.map((neighbourhood, index) => (
-        <option key={index} value={neighbourhood}>{neighbourhood}</option>
-      ))}
-    </select>
-  );
-};
-
-export default NeighbourhoodFilter;
+// export default NeighbourhoodFilter;
---
+++ components/TimeFilter/TimeFilter.jsx
@@ -0,0 +1,21 @@
+// import React from "react";
+
+// const TimeFilter = ({ onTimePeriodChange }) => {
+//   const handleTimePeriodChange = (event) => {
+//     onTimePeriodChange(event.target.value);
+//   };
+
+//   return (
+//     <div>
+//       <label htmlFor="timePeriod">Select Time Period:</label>
+//       <select id="timePeriod" onChange={handleTimePeriodChange}>
+//         <option value="">--Select Time Period--</option>
+//         <option value="morn">Morning (Before 12 PM)</option>
+//         <option value="aft">Afternoon (12 PM - 6 PM)</option>
+//         <option value="eve">Evening (After 6 PM)</option>
+//       </select>
+//     </div>
+//   );
+// };
+
+// export default TimeFilter;
