415b0af1f677a4db7b2f45fb851fb11a0953a7e6
Jonathan Mazin <jmazin@brainstation.io>
2025-03-19 13:49:21 -0400

    pair /w Jon: get comment form back

--- pages/VenueDealsPage/VenueDealsPage.jsx
+++ pages/VenueDealsPage/VenueDealsPage.jsx
@@ -2,32 +2,31 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import VenueDetails from "../../components/VenueDetails/VenueDetails";
-// import CommentForm from "../../components/CommentForm/CommentForm";
-// import CommentsDisplay from "../../components/CommentsDisplay/CommentsDisplay";
+import CommentForm from "../../components/CommentForm/CommentForm";
+import CommentsDisplay from "../../components/CommentsDisplay/CommentsDisplay";
 
 function VenueDealsPage() {
   const { id } = useParams();
   const [comments, setComments] = useState([]);
 
-  async function fetchComments() {
-  //   try {
-  //     const response = await axios.get(`http://localhost:8080/venues/${id}/comments`);
-  //     setComments(response.data);
-  //   } catch (error) {
-  //     console.error("Error fetching comments:", error);
-  //   }
-  // }
+  useEffect(() => {
+    fetchComments();
+  }, [id]);
 
-  // useEffect(() => {
-  //   fetchComments();
-  // }, [id]);
+  async function fetchComments() {
+    try {
+      const response = await axios.get(`http://localhost:8080/venues/${id}/comments`);
+      setComments(response.data);
+    } catch (error) {
+      console.error("Error fetching comments:", error);
+    }
   }
 
   return (
     <>
       <VenueDetails id={id} />
-      {/* <CommentForm fetchComments={fetchComments} id={id} /> */}
-      {/* <CommentsDisplay comments={comments} id={id} /> */}
+      <CommentForm fetchComments={fetchComments} id={id} />
+      <CommentsDisplay comments={comments} id={id} />
     </>
   );
 }
