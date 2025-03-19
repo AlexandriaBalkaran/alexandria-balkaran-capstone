10fb80f747c09df23032b7539595e1669ede0a03
Alexandria Balkaran <alexandriabalkaran@gmail.com>
2025-03-19 16:44:49 -0400

    neighbourhood filter styling

--- components/NeighbourhoodFilter/NeighbourhoodFilter.jsx
+++ components/NeighbourhoodFilter/NeighbourhoodFilter.jsx
@@ -1,15 +1,28 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 import "./NeighbourhoodFilter.scss";
 
-const NeighbourhoodFilter = ({ neighbourhoods, selectedNeighbourhood, onFilterChange }) => {
+const NeighbourhoodFilter = ({ neighbourhoods, selectedNeighbourhoods, onNeighbourhoodClick, toggleNeighbourhoods, showNeighbourhoods }) => {
   return (
-    <select value={selectedNeighbourhood} onChange={onFilterChange}>
-      <option value="">All Neighbourhoods</option>
-      {neighbourhoods.map((neighbourhood, index) => (
-        <option key={index} value={neighbourhood}>{neighbourhood}</option>
-      ))}
-    </select>
+    <div className="neighbourhood-filter">
+      <button className="neighbourhood-filter__toggle" onClick={toggleNeighbourhoods}>
+        All Neighbourhoods
+      </button>
+      {showNeighbourhoods && (
+        <div className="neighbourhood-filter__buttons">
+          {neighbourhoods.map((neighbourhood) => (
+            <button
+              key={neighbourhood}
+              className={`neighbourhood-filter__button ${
+                selectedNeighbourhoods.includes(neighbourhood) ? "selected" : ""
+              }`}
+              onClick={() => onNeighbourhoodClick(neighbourhood)}
+            >
+              {neighbourhood}
+            </button>
+          ))}
+        </div>
+      )}
+    </div>
   );
 };
 
--- pages/LocationPage/LocationPage.jsx
+++ pages/LocationPage/LocationPage.jsx
@@ -1,58 +1,7 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import axios from "axios";
-// import "./LocationPage.scss";
-// import React, { useEffect, useState } from "react";
-// import VenueCard from "../../components/VenueCard/VenueCard";
-// import NeighbourhoodFilter from "../../components/NeighbourhoodFilter/NeighbourhoodFilter";
-
-// function LocationPage() {
-//   const [venues, setVenues] = useState([]);
-//   const [neighbourhoods, setNeighbourhoods] = useState([]);
-//   const [selectedNeighbourhood, setSelectedNeighbourhood] = useState("");
-
-//   useEffect(() => {
-//     fetchVenues();
-//   }, []);
-
-//   const fetchVenues = async () => {
-//     try {
-//       const response = await fetch("http://localhost:8080/venues");
-//       const data = await response.json();
-//       setVenues(data);
-//       setNeighbourhoods([...new Set(data.map(venue => venue.neighbourhood))]);
-//     } catch (e) {
-//       console.error("Error fetching venues:", e);
-//     }
-//   };
-
-//   const handleFilterChange = (event) => {
-//     setSelectedNeighbourhood(event.target.value);
-//   };
-
-//   const filteredVenues = selectedNeighbourhood
-//     ? venues.filter(venue => venue.neighbourhood === selectedNeighbourhood)
-//     : venues;
-
-//   return (
-//     <div>
-//            <NeighbourhoodFilter 
-//         neighbourhoods={neighbourhoods} 
-//         selectedNeighbourhood={selectedNeighbourhood} 
-//         onFilterChange={handleFilterChange} 
-//       />
-//       <VenueCard venues={filteredVenues} />
-//     </div>
-//   );
-// }
-
-// export default LocationPage;
-
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import axios from "axios";
-import "./LocationPage.scss";
 import React, { useEffect, useState } from "react";
+import "./LocationPage.scss";
 import VenueCard from "../../components/VenueCard/VenueCard";
+import NeighbourhoodFilter from "../../components/NeighbourhoodFilter/NeighbourhoodFilter";
 
 function LocationPage() {
   const [venues, setVenues] = useState([]);
@@ -69,7 +18,7 @@ function LocationPage() {
       const response = await fetch("http://localhost:8080/venues");
       const data = await response.json();
       setVenues(data);
-      setNeighbourhoods([...new Set(data.map(venue => venue.neighbourhood))]);
+      setNeighbourhoods([...new Set(data.map((venue) => venue.neighbourhood))]);
     } catch (e) {
       console.error("Error fetching venues:", e);
     }
@@ -80,35 +29,29 @@ function LocationPage() {
   };
 
   const handleNeighbourhoodClick = (neighbourhood) => {
-    setSelectedNeighbourhoods(prevSelected => {
-      if (prevSelected.includes(neighbourhood)) {
-        return prevSelected.filter(n => n !== neighbourhood);
-      } else {
-        return [...prevSelected, neighbourhood];
-      }
-    });
+    setSelectedNeighbourhoods((prevSelected) =>
+      prevSelected.includes(neighbourhood)
+        ? prevSelected.filter((n) => n !== neighbourhood)
+        : [...prevSelected, neighbourhood]
+    );
   };
 
-  const filteredVenues = selectedNeighbourhoods.length > 0
-    ? venues.filter(venue => selectedNeighbourhoods.includes(venue.neighbourhood))
-    : venues;
+  const filteredVenues =
+    selectedNeighbourhoods.length > 0
+      ? venues.filter((venue) =>
+          selectedNeighbourhoods.includes(venue.neighbourhood)
+        )
+      : venues;
 
   return (
-    <div>
-      <button onClick={toggleNeighbourhoods}>All Neighbourhoods</button>
-      {showNeighbourhoods && (
-        <div className="neighbourhood-buttons">
-          {neighbourhoods.map(neighbourhood => (
-            <button
-              key={neighbourhood}
-              className={`neighbourhood-button ${selectedNeighbourhoods.includes(neighbourhood) ? 'selected' : ''}`}
-              onClick={() => handleNeighbourhoodClick(neighbourhood)}
-            >
-              {neighbourhood}
-            </button>
-          ))}
-        </div>
-      )}
+    <div className="location-page">
+      <NeighbourhoodFilter
+        neighbourhoods={neighbourhoods}
+        selectedNeighbourhoods={selectedNeighbourhoods}
+        onNeighbourhoodClick={handleNeighbourhoodClick}
+        toggleNeighbourhoods={toggleNeighbourhoods}
+        showNeighbourhoods={showNeighbourhoods}
+      />
       <VenueCard venues={filteredVenues} />
     </div>
   );
