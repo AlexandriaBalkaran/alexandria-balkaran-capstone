7c27e29a5f59ae4bfbc592f6a012fd4e094a0287
Alexandria Balkaran <alexandriabalkaran@gmail.com>
2025-03-19 09:08:21 -0400

    add venue card styling

--- components/VenueCard/VenueCard.jsx
+++ components/VenueCard/VenueCard.jsx
@@ -3,24 +3,24 @@ import { Link } from "react-router-dom";
 import "./VenueCard.scss";
 
 function VenueCard({ venue }) {
+  if (!venue) {
+    return <div>No venue available</div>;
+  }
 
-    if (!venue) {
-        return <div>No venue available</div>;
-      }
   return (
     <Link to={`/venues/${venue.id}`} className="card__link">
-      <div className="card__container" key={photo.id}>
+      <div className="card__container" key={venue.id}>
         <img
           className="card__image"
           src={venue.photo}
-        //   alt={photo.photoDescription}
+          alt={venue.name || "Venue image"}
         />
-        <h4 className="card__photographer">{photo.photographer}</h4>
-        {/* <div className="tag__container">
-        {photo.tags.map((tag) => (
-          <Tag tag={tag} key={tag} />
-        ))}
-        </div> */}
+        <h4 className="card__name">{venue.name}</h4>
+        <div className="tag__container">
+          {venue.days && venue.days.map((day) => (
+            <span className="tag" key={day}>{day}</span>
+          ))}
+        </div>
       </div>
     </Link>
   );
