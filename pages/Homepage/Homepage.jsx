468968da3b458557555ecddd629ebee94202f76f
Alexandria Balkaran <alexandriabalkaran@gmail.com>
2025-03-19 20:40:58 -0400

    font changed and margins added

--- pages/Homepage/Homepage.jsx
+++ pages/Homepage/Homepage.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import axios from "axios";
 import "./Homepage.scss";
 import React, { useEffect, useState } from "react";
+import HomePageText from "../../components/HomePageText/HomePageText";
 
 function HomePage() {
   const [venues, setVenues] = useState([]);
@@ -34,48 +35,7 @@ function HomePage() {
 
   return (
     <div>
-      <h2>Welcome to Pour Decisions 🍸</h2>
-      <p>Your Guide to the Best Happy Hour Deals in Toronto!</p>
-      <p>
-        Why spend hours searching for the best happy hour specials when you can
-        find them all in one place? Pour Decisions is your go-to web app for
-        discovering the best drink and food deals in downtown Toronto – all
-        based on your location, date, and time of day. Whether you're looking
-        for an afternoon pint, evening cocktails, or late-night bites, we've got
-        you covered! 🍻🍹
-      </p>
-
-      <h4>How It Works</h4>
-      <p>
-        ✅ Find Deals Instantly – Browse happy hour specials by neighbourhood,
-        time, and date
-      </p>
-      <p>
-        ✅ Save Time & Money – No more endless Google searches or
-        overpaying for drinks 💰 
-      </p>
-      <p>
-        ✅ Unbiased Reviews – Read and leave reviews on
-        drinks, food, atmosphere, and experience 
-      </p>
-      <p>
-        ✅ Drink First, Think Later –
-        Just open our site and let the deals come to you! 🍸
-      </p>
-      <h4>Why Pour Decisions?</h4>
-      <p>
-        With the cost of living on the rise, every dollar counts. We believe
-        affordable drinks and good times should be easy to find. Whether you're
-        a cocktail connoisseur, beer enthusiast, or foodie looking for the best
-        bites, we make happy hour hunting effortless. 
-      </p>
-      <p>
-        💡 Stop Googling, start drinking – discover Toronto’s best happy hour deals today!
-      </p>
-      <img className="skyline__image"
-              src="/src/assets/images/homepage-skyline-pd.svg"
-              alt="skyline of Toronto cartoon"
-        ></img>
+      <HomePageText/>
     </div>
   );
 }
