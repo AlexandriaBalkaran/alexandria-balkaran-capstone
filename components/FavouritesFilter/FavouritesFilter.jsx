33845e11f193bb719392c5da401b957e5fed7f8c
Alexandria Balkaran <alexandriabalkaran@gmail.com>
2025-03-20 15:09:42 -0400

    can unlike favourites from FavouritesPage

--- components/FavouritesFilter/FavouritesFilter.jsx
+++ components/FavouritesFilter/FavouritesFilter.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import VenueCard from "../VenueCard/VenueCard";
 
 function FavouritesFilter() {
-  const favourites = JSON.parse(localStorage.getItem("favourites")) || [];
+  const [favourites, setFavourites] = useState([]);
+
+  useEffect(() => {
+    const savedFavourites = JSON.parse(localStorage.getItem("favourites")) || [];
+    setFavourites(savedFavourites);
+  }, []);
+
+  const handleFavouriteClick = (venue) => {
+    setFavourites((prevFavourites) => {
+      const updatedFavourites = prevFavourites.some((fav) => fav.id === venue.id)
+        ? prevFavourites.filter((fav) => fav.id !== venue.id)
+        : [...prevFavourites, venue];
+
+      localStorage.setItem("favourites", JSON.stringify(updatedFavourites));
+      return updatedFavourites;
+    });
+  };
 
   return (
     <div>
       <h2>My Favourites</h2>
-      {favourites.length === 0 ? <p>No favourites yet!</p> : <VenueCard venues={favourites} favourites={favourites} />}
+      {favourites.length === 0 ? (
+        <p>No favourites yet!</p>
+      ) : (
+        <VenueCard venues={favourites} favourites={favourites} onFavouriteClick={handleFavouriteClick} />
+      )}
     </div>
   );
 }
