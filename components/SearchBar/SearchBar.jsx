e53d48cd39763969a5c35d1005ada2926a88e7d0
Alexandria Balkaran <alexandriabalkaran@gmail.com>
2025-03-22 16:30:15 -0400

    font sizes for breakpoints

--- components/SearchBar/SearchBar.jsx
+++ components/SearchBar/SearchBar.jsx
@@ -4,15 +4,15 @@ import "./SearchBar.scss";
 function SearchBar({ searchTerm, onSearchChange }) {
   return (
     <div className="search-bar__container">
-          <input
-      type="text"
-      placeholder="Search venues or neighbourhoods"
-      value={searchTerm}  
-      onChange={onSearchChange}
-      className="search-bar"
-    />
+      <input
+        type="text"
+        placeholder="Search venues or neighbourhoods"
+        value={searchTerm}
+        onChange={onSearchChange}
+        className="search-bar"
+      />
     </div>
-     );
+  );
 }
 
 export default SearchBar;
